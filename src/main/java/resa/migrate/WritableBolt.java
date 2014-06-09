2edb37b051a137f4c47382b6927ff090aa0bc284
troyding <dingsword@gmail.com>
2014-06-09 17:13:42 +0800

    add migrate package

--- src/main/java/resa/migrate/WritableBolt.java
+++ src/main/java/resa/migrate/WritableBolt.java
@@ -1,8 +1,11 @@
 package resa.migrate;
 
 import backtype.storm.serialization.SerializationFactory;
+import backtype.storm.task.IBolt;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicBoltExecutor;
+import backtype.storm.topology.IBasicBolt;
 import backtype.storm.topology.IRichBolt;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Output;
@@ -11,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import resa.topology.DelegatedBolt;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,6 +30,7 @@ public class WritableBolt extends DelegatedBolt {
 
     private transient Map<String, Object> conf;
     private transient Kryo kryo;
+    private transient Persistable persistableBolt;
 
     public WritableBolt(IRichBolt delegate) {
         super(delegate);
@@ -35,22 +40,49 @@ public class WritableBolt extends DelegatedBolt {
     public void prepare(Map conf, TopologyContext context, OutputCollector outputCollector) {
         super.prepare(conf, context, outputCollector);
         this.conf = conf;
-        IRichBolt bolt = super.getDelegate();
-        if (bolt instanceof Persistable) {
+        // find the last bolt, and check whether it is persistable
+        persistableBolt = getPersistableObject();
+        if (persistableBolt != null) {
             kryo = SerializationFactory.getKryo(conf);
-            loadData((Persistable) bolt);
+            LOG.info("Bolt " + persistableBolt.getClass() + " support persist");
+            loadData();
         } else {
             kryo = null;
+            LOG.info("No persistable bolt is found");
         }
-        context.registerMetric("serialized", this::getSerializedSize, 300);
+        context.registerMetric("serialized", this::getSerializedSize, 30);
+    }
+
+    private Persistable getPersistableObject() {
+        IBolt bolt = this.getDelegate();
+        // find the last bolt
+        while (bolt instanceof DelegatedBolt) {
+            if (bolt instanceof Persistable) {
+                break;
+            }
+            bolt = ((DelegatedBolt) bolt).getDelegate();
+        }
+        if (bolt instanceof Persistable) {
+            return (Persistable) bolt;
+        } else if (bolt instanceof BasicBoltExecutor) {
+            try {
+                Field f = BasicBoltExecutor.class.getDeclaredField("_bolt");
+                f.setAccessible(true);
+                IBasicBolt basicBolt = (IBasicBolt) f.get(bolt);
+                return basicBolt instanceof Persistable ? (Persistable) basicBolt : null;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        return null;
     }
 
     private Long getSerializedSize() {
         Long size = null;
-        if (kryo != null) {
+        if (persistableBolt != null) {
             Path file = Paths.get(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
             try (Output out = new Output(Files.newOutputStream(file))) {
-                ((Persistable) super.getDelegate()).save(kryo, out);
+                persistableBolt.save(kryo, out);
             } catch (IOException e) {
                 LOG.warn("Save bolt failed", e);
             }
@@ -63,9 +95,9 @@ public class WritableBolt extends DelegatedBolt {
         return size;
     }
 
-    private void loadData(Persistable bolt) {
+    private void loadData() {
         try {
-            bolt.read(SerializationFactory.getKryo(conf), null);
+            persistableBolt.read(SerializationFactory.getKryo(conf), null);
         } catch (IOException e) {
             LOG.warn("Load bolt failed", e);
         }
@@ -74,9 +106,9 @@ public class WritableBolt extends DelegatedBolt {
     @Override
     public void cleanup() {
         super.cleanup();
-        if (kryo != null) {
+        if (persistableBolt != null) {
             try {
-                ((Persistable) super.getDelegate()).save(kryo, null);
+                persistableBolt.save(kryo, null);
             } catch (IOException e) {
                 LOG.warn("Save bolt failed", e);
             }
