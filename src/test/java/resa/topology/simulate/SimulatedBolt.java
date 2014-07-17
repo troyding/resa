2869bdb467699e0e736811430ece5640c9f34d66
troyding <dingsword@gmail.com>
2014-07-17 16:01:24 +0800

    bug fix

--- src/test/java/resa/topology/simulate/SimulatedBolt.java
+++ src/test/java/resa/topology/simulate/SimulatedBolt.java
@@ -3,6 +3,7 @@ package resa.topology.simulate;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 
@@ -11,25 +12,31 @@ import java.util.Map;
 /**
  * Created by ding on 14-7-1.
  */
-public class SimulatedBolt extends TASleepBolt {
+public class SimulatedBolt extends BaseRichBolt {
 
-    public SimulatedBolt(long computeTime) {
-        this.computeTime = computeTime;
+    public SimulatedBolt(double lambda) {
+        this.lambda = lambda;
     }
 
     private OutputCollector collector;
-    private long computeTime;
+    private double lambda;
+
 
     @Override
     public void prepare(Map map, TopologyContext context, OutputCollector outputCollector) {
-        super.prepare(map, context, outputCollector);
         this.collector = outputCollector;
-        setIntervalSupplier(() -> computeTime);
     }
 
     @Override
     public void execute(Tuple tuple) {
-        super.execute(tuple);
+        long cost;
+        long exp = (long) (-Math.log(Math.random()) * 1000000000 / lambda) + 1;
+        long now = System.nanoTime();
+        do {
+            for (int i = 0; i < 10; i++) {
+                Math.sqrt(Math.random() * Integer.MAX_VALUE);
+            }
+        } while ((cost = (System.nanoTime() - now)) > 0 && cost < exp);
         collector.emit(tuple.getValues());
         collector.ack(tuple);
     }
